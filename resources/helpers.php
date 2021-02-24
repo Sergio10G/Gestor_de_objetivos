6bd7f833326e13eb35a5c2f1c14c65a4d0752c4c
Sergio10G <sejobmx@hotmail.es>
2021-02-24 18:10:56 +0100

    Añadida la vista de los objetivos, faltan por definir sus reglas de estilo. Añadidas las funciones invertir fecha y fecha usable a helpers

--- resources/helpers.php
+++ resources/helpers.php
@@ -1,2 +1,10 @@
 <?php
 
+function fecha_eliminar_hora($fecha_con_hora){
+    return explode(" ", $fecha_con_hora)[0];
+}
+
+function fecha_invertir($fecha){
+    $partes = explode("-", $fecha);
+    return $partes[2]."-".$partes[1]."-".$partes[0];
+}
